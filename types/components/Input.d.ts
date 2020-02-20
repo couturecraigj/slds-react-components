762ed67767a827f05a3e7cd7b790ad123b8350dd
Craig Couture <couturecraigj@gmail.com>
2020-02-20 13:15:23 -0600

    Input Type Defs

--- types/components/Input.d.ts
+++ types/components/Input.d.ts
@@ -2,12 +2,12 @@
 declare const Input: ({ children, label, name, id, type, required, placeholder, value, ...props }: {
     children?: any;
     label: string;
-    name: string;
-    id: string;
-    type: string;
-    required: boolean;
-    placeholder: string;
-    value: any;
+    name?: string | undefined;
+    id?: string | undefined;
+    type?: string | undefined;
+    required?: boolean | undefined;
+    placeholder?: string | undefined;
+    value?: any;
 }) => JSX.Element;
 export default Input;
 //# sourceMappingURL=Input.d.ts.map
