250171bb00571684612030230514a86d3bf7166c
Craig Couture <couturecraigj@gmail.com>
2020-02-28 09:31:13 -0600

    Adding SLDSContext

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as getWindowHeight } from "./getWindowHeight";
 export { default as useLocation } from "./useLocation";
+export { default as useSldsContext } from "./useSldsContext";
 export { default as useLCC } from "./useLCC";
---
+++ types/hooks/useSldsContext.d.ts
@@ -0,0 +1,4 @@
+import { contextArguments } from '../components/Context';
+declare const useSldsContext: () => contextArguments;
+export default useSldsContext;
+//# sourceMappingURL=useSldsContext.d.ts.map
